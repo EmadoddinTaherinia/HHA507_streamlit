bffead3528a1546b1356bfe4ea5a37995bfe00ad
EmadoddinTaherinia <imadlldin@gmail.com>
2021-12-16 19:44:27 -0500

    Update ETstreamlit.py

--- ETstreamlit.py
+++ ETstreamlit.py
@@ -5,9 +5,9 @@ Created on Thu Dec 16 19:06:06 2021
 @author: Emad
 """
 
-!pip install streamlit
+pip install streamlit
 import pandas as pd
 import numpy as np
 
-st.title ('HHA507 Fall 2021 Final assignment')
+st.title('HHA507 Fall 2021 Final assignment')
 
