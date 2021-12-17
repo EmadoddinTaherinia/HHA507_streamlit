f59a3263cd6eb69fd593e6be5530a7053b46b249
EmadoddinTaherinia <imadlldin@gmail.com>
2021-12-16 20:18:46 -0500

    Update ETstreamlit.py

--- ETstreamlit.py
+++ ETstreamlit.py
@@ -8,6 +8,11 @@ Created on Thu Dec 16 19:06:06 2021
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import time
+
 
 st.title('HHA507-Fall-2021-Final-assignment')
 
